e372b62fb0e53a729004bddaeba2246ef31c24c2
ct-chou <63345621+ct-chou@users.noreply.github.com>
2025-03-04 17:40:45 -0500

    Feat: created factory function for Ship

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-function sum(a,b) {
-    return a+ b;
+// direction: horizontal or vertical
+function Ship (length, direction) {
+    let hits = 0;
+
+    const hit = () => {
+        hits++;
+        return true;
+    }
+
+    const isSunk = () => {
+        if(hits >= length) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return {length, direction, hit, isSunk};
 }
 
-module.exports = sum;
+const ship1 = Ship(2, 'horizontal');
+
+module.exports = ship1;
