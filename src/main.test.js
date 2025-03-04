deaf395add804b3387a9118c53ba78e6f35a7703
ct-chou <63345621+ct-chou@users.noreply.github.com>
2025-03-04 18:56:29 -0500

    Feat: setup gameboard factory function - can place ships and receive attacks but not tracking misses and hits. Also need to report if all ships are sunk

--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,31 @@
-const ship1 = require('./main.js');
+const {ship, gameboard} = require('./main.js');
 
 test('Ship functions', () => {
+    const ship1 = ship(2,'horizontal');
     ship1.hit();
     expect(ship1.isSunk()).toBe(false);
     ship1.hit();
     expect(ship1.isSunk()).toBe(true);
 });
+
+test('Gameboard', () => {
+    let x_coord = 0;
+    let y_coord = 3;
+    const gameboard1 = gameboard(10);
+    const ship1 = ship(2,'horizontal');
+    expect(gameboard1.place(ship1, x_coord, y_coord)).toBe(true);
+    const ship3 = ship(3,'horizontal');
+    expect(gameboard1.place(ship3, x_coord, y_coord + 1)).toBe(true);
+    expect(gameboard1.place(ship3, x_coord + 12, y_coord)).toBe(false);
+});
+
+test('Receive Attack', () => {
+    let x_coord = 0;
+    let y_coord = 3;
+    const gameboard1 = gameboard(10);
+    const ship1 = ship(2,'horizontal');
+    expect(gameboard1.place(ship1, x_coord, y_coord)).toBe(true);
+    expect(gameboard1.receiveAttack(0,3)).toBe('hit');
+    expect(gameboard1.receiveAttack(1,3)).toBe('sunk ship');
+    expect(gameboard1.receiveAttack(0,2)).toBe('miss');
+})
